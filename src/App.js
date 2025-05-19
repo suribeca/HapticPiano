6c6dca315f3a58d2bcbd5dcd71c713972a142952
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-05-19 15:55:42 -0600

    Update App.js
    App.js con menú ahora siendo la página principal, demo y una página de práctica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import { Piano } from './components/Piano.js';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Practica from './pages/Practica';
+import { Piano } from './components/Piano';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Piano />
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/demo" component={Piano} />
+        <Route path="/practica" component={Practica} />
+      </Switch>
+    </Router>
   );
 }
 
