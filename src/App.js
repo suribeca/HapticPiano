2a227708808521318a2c1a83949251d83e20c8c4
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-05-19 21:37:54 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Practica from './pages/Practica';
 import { Piano } from './components/Piano';
@@ -7,13 +7,14 @@ import { Piano } from './components/Piano';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/demo" component={Piano} />
-        <Route path="/practica" component={Practica} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Piano />} />
+        <Route path="/practica" element={<Practica />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
