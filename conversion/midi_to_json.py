8144bf1cadfde3c7527aff6dce89d85569d1779e
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-06-25 21:55:50 -0600

    Update midi_to_json.py
    Comments about the conversion process

--- conversion/midi_to_json.py
+++ conversion/midi_to_json.py
@@ -1,10 +1,17 @@
+# Requiere de pip install mido
+#Clase para convertir archivos MIDI en JSON tomando el nombre de la nota (en su valor) 
+#   y el tiempo en que comienza cada nota en ser presionada 
 from mido import MidiFile
 import json
 
-mid = MidiFile('odeDifficult.midi')
-events = []
+mid = MidiFile('odeDifficult.midi') # Poner el archivo para hacer la conversión de MIDI -> JSON
+events = [] # Arreglo donde vamos a poner todos los eventos MIDI
 time = 0
 
+# Extrae los eventos note_on con una velocidad mayor a 0, a un arreglo JSON
+# En MIDI (colección de tracks) cada evento tiene información de 
+    # Lo que está ocurriendo
+    # El tiempo que pasó desde el último evento 
 for track in mid.tracks:
     for msg in track:
         time += msg.time
